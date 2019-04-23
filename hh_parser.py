2f93d87f78c3d1da22cbac97a33c94742d61d06b
plasch <latrodectuso@gmail.com>
2019-04-23 22:06:35 +0300

    Created hh.ru parser

---
+++ hh_parser.py
@@ -0,0 +1,34 @@
+import requests
+from bs4 import BeautifulSoup as bs
+
+headers = {'accept': '*/*',
+           'user-agent': 'Mozilla/5.0(X11;Linux x86_64...)Geco/20100101 Firefox/60.0'}
+base_url = 'https://hh.ru/search/vacancy?search_period=3&text=python&area=1&page=0'
+
+
+def hh_parse(base_url, headers):
+    jobs = []
+    session = requests.session()
+    request = session.get(base_url, headers=headers)
+    if request.status_code == 200:
+        soup = bs(request.content, 'html.parser')
+        divs = soup.find_all('div', attrs={'data-qa': 'vacancy-serp__vacancy'})
+        for div in divs:
+            title = div.find('a', attrs={'data-qa': 'vacancy-serp__vacancy-title'}).text
+            href = div.find('a', attrs={'data-qa': 'vacancy-serp__vacancy-title'})['href']
+            company = div.find('a', attrs={'data-qa': 'vacancy-serp__vacancy-employer'}).text
+            text1 = div.find('div', attrs={'data-qa': 'vacancy-serp__vacancy_snippet_responsibility'}).text
+            text2 = div.find('div', attrs={'data-qa': 'vacancy-serp__vacancy_snippet_requirement'}).text
+            content = text1 + ' ' + text2
+            jobs.append({
+                'title': title,
+                'href': href,
+                'company': company,
+                'content': content,
+            })
+        print(jobs)
+    else:
+        print('ERROR')
+
+
+hh_parse(base_url, headers)
